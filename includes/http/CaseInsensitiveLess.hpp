cd539a0b991fea0465a2fae0b506d1f5a8e624c0
rbagin <ravi.bagin@gmail.com>
2026-08-27 19:35:15 +0200

    WIP:added all of my stashed work, still not complete

---
+++ includes/http/CaseInsensitiveLess.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+# include <string>
+# include <cctype>
+# include <string_view>
+
+struct CaseInsensitiveLess { // a struct for normalizing strings in the map
+	bool operator()(std::string_view a, std::string_view b) const
+	{
+		size_t n = a.size() < b.size() ? a.size() : b.size();
+		for (size_t i = 0; i < n; ++i) {
+			unsigned char x = tolower(static_cast<unsigned char>(a[i]));
+			unsigned char y = tolower(static_cast<unsigned char>(b[i]));
+			if (x != y)
+				return x < y;
+		}
+		return a.size() < b.size();
+	}
+};
---
+++ includes/http/HttpResponse.hpp
@@ -0,0 +1,23 @@
+#ifndef HTTPRESPONSE_HPP
+# define HTTPRESPONSE_HPP
+
+# include <string>
+# include <map>
+# include "CaseInsensitiveLess.hpp"
+
+struct HttpResponse {
+	int statusCode = 200;
+	std::string statusText;
+	std::map<std::string, std::string, CaseInsensitiveLess> headers;
+	std::string body;
+	static HttpResponse make(int code, std::string body = "");
+	static HttpResponse makeError(int code);
+	static std::string reasonFor(int code);
+	void setHeader(const std::string &name,const std::string &value);
+	std::string getHeader(const std::string &name) const;
+	bool hasHeader(const std::string &name) const;
+};
+
+std::string toBytes(const HttpResponse& response);
+
+#endif
