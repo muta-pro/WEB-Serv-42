3722477305083172fe0c8144934e4a8dc39ea87c
lizafotkaet <elizavetabarbashh@gmail.com>
2026-08-27 17:23:34 +0200

    httprequest.hpp changes/parseresult.hpp added

--- includes/http/HttpRequest.hpp
+++ includes/http/HttpRequest.hpp
@@ -2,20 +2,37 @@
 
 #include <string>
 #include <map>
+#include <string_view>
+#include <cctype> // added a header
+
+struct CaseInsensitiveLess { // a struct for normalizing strings in the map
+	bool operator()(const std::string& a, const std::string& b) const {
+		size_t n = a.size() < b.size() ? a.size() : b.size();
+		for (size_t i = 0; i < n; ++i) {
+			unsigned char x = tolower(static_cast<unsigned char>(a[i]));
+			unsigned char y = tolower(static_cast<unsigned char>(b[i]));
+			if (x != y)
+				return x < y;
+		}
+		return a.size() < b.size();
+	}
+};
+
 
 struct HttpRequest {
-	std::string							method;  // GET POST DELETE
-	std::string							path;    // /index.html
-	std::string							version; // HTTP/1.1
-	std::map<std::string,std::string>	headers;
-	std::string							body;
+	std::string_view							method;  // GET POST DELETE
+	std::string_view							path;    // /index.html
+	std::string_view							version; // HTTP/1.1
+	std::map<std::string_view,std::string_view, CaseInsensitiveLess>	headers;
+	std::string_view							body;
 
-	std::string							header(const std::string& k) const;
-	size_t								contentLength() const;
-	bool								isKeepAlive() const;
-} ;
+	std::string_view							header(const std::string_view k) const;
+	size_t										contentLength() const;
+	bool										isKeepAlive() const;
+};
 
-/* I don't know what else to put here yet, this struct goes inside
-connection (according to the contracts page of the plan) - Liza
+/* 
+
+this is gonna most probably need major changes later - Liza
 
 */
---
+++ includes/http/ParseResult.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "HttpRequest.hpp"
+#include <string_view>
+
+enum class ParseStatus {
+	INCOMPLETE, 
+	COMPLETE, 
+	ERROR
+};
+
+struct ParseResult { // to be figured out later - Liza
+	ParseStatus	status;
+	size_t		bytesConsumed;
+	int			errorCode;
+	std::string	errorMsg;
+};
