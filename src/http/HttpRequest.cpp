484129dc3554f53307caa00a5ba0e3a81ad0cc2f
lizafotkaet <elizavetabarbashh@gmail.com>
2026-08-30 02:15:28 +0200

    added ParseResult.cpp file and put HttpRequestParser member functions' prototypes in there; put HttpRequest member functions prototypes in HttpRequest.cpp

--- src/http/HttpRequest.cpp
+++ src/http/HttpRequest.cpp
@@ -1,2 +1,15 @@
 #include "http/HttpRequest.hpp"
 
+// to be implemented
+
+std::string	HttpRequest::header(const std::string k) const{
+
+}
+
+size_t		HttpRequest::contentLength() const{
+
+}
+
+bool		HttpRequest::isKeepAlive() const{
+
+}
